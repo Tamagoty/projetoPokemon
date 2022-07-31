ecbd968bd6d08df007c2a6595e26bd68d1dafbd1
Tamagoty <79391511+Tamagoty@users.noreply.github.com>
2022-07-31 01:55:11 -0300

    projeto finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,30 @@
 const btnProximo = document.getElementById('btnProximo');
 const btnAnterior = document.getElementById('btnAnterior');
 const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
 
 btnProximo.addEventListener('click', function () {
-    console.log(cartoes);
-})
+    if (cartaoAtual < cartoes.length - 1) {
+        cartoes[cartaoAtual].classList.remove('selecionado')
+        cartaoAtual++;
+        cartoes[cartaoAtual].classList.add('selecionado');
+        if (cartaoAtual === cartoes.length - 1) {
+            document.getElementById('btnProximo').classList.remove('main-btn-ativo');
+        }
+        document.getElementById('btnAnterior').classList.add('main-btn-ativo');
+    }
+}
+)
+
+btnAnterior.addEventListener('click', function () {
+    if (cartaoAtual > 0) {
+        cartoes[cartaoAtual].classList.remove('selecionado')
+        cartaoAtual--;
+        cartoes[cartaoAtual].classList.add('selecionado');
+        if (cartaoAtual === 0) {
+            document.getElementById('btnAnterior').classList.remove('main-btn-ativo');
+        }
+        document.getElementById('btnProximo').classList.add('main-btn-ativo');
+    }
+}
+)
