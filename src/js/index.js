6e0883ebd0574b3b640a104c9b0e918d1f7c194d
Tamagoty <79391511+Tamagoty@users.noreply.github.com>
2022-07-31 00:44:03 -0300

    Correção de fonte

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const btnProximo = document.getElementById('btnProximo');
+const btnAnterior = document.getElementById('btnAnterior');
+const cartoes = document.querySelectorAll('.cartao');
+
+btnProximo.addEventListener('click', function () {
+    console.log(cartoes);
+})
